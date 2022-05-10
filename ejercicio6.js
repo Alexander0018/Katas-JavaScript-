138b756facd7591cd8ca5ae3d9b15c1f787bcc8f
Frank Alexander Ardila Tunubala <frankardila@api.docker-mdclaim.com>
2022-05-10 15:21:26 -0500

    ejercicio6

---
+++ ejercicio6.js
@@ -0,0 +1,14 @@
+/*
+
+https://www.codewars.com/kata/523b4ff7adca849afe000035/train/javascript
+
+Function 1-hello world
+8 kyu
+*/
+
+function greet(str){
+  
+    str= 'hello world!';
+    return str;
+    
+  };
