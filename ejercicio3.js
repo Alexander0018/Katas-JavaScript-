7d167a0c8cba03387f86d269546212008340d2b3
Frank Alexander Ardila Tunubala <frankardila@api.docker-mdclaim.com>
2022-05-10 13:59:18 -0500

    ejercicio3

---
+++ ejercicio3.js
@@ -0,0 +1,19 @@
+/*
+
+  https://www.codewars.com/kata/551b4501ac0447318f0009cd/train/javascript
+
+  Convert a Boolean to a String
+
+  8 kyu
+
+
+
+
+
+*/
+
+let b= 'true';
+function booleanToString(b){
+  var bo= b.toString();
+  return bo;
+}
