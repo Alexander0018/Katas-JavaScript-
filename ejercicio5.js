60b61388bb22abb991881923828c796939de850c
Frank Alexander Ardila Tunubala <frankardila@api.docker-mdclaim.com>
2022-05-10 15:01:30 -0500

    ejercicio5

---
+++ ejercicio5.js
@@ -0,0 +1,23 @@
+/*
+
+https://www.codewars.com/kata/57e76bc428d6fbc2d500036d/train/javascript
+
+Convert a string to an array
+
+8 kyu
+
+
+
+
+
+*/ 
+
+
+function stringToArray(str){
+ 
+    let str2= str.split(" ");
+    return str2;
+    
+  
+   
+  }
