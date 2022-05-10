019c7a25307d54a4fb07ea624259e655e6fdcb64
Frank Alexander Ardila Tunubala <frankardila@api.docker-mdclaim.com>
2022-05-10 11:29:01 -0500

    primero

---
+++ ejercicio1.js
@@ -0,0 +1,15 @@
+/*
+
+https://www.codewars.com/kata/5265326f5fda8eb1160004c8
+Convert a number to String
+
+8 kyu
+
+*/ 
+
+function numberToString(num) {
+    var nume=String(num);
+  
+   return nume;
+    // Return a string of the number here!
+  }
