bbc5e4567fdf0bf595a495c627f5288916b0fd62
Frank Alexander Ardila Tunubala <frankardila@api.docker-mdclaim.com>
2022-05-10 13:41:23 -0500

    segundo ejercicio

---
+++ ejercicio2.js
@@ -0,0 +1,23 @@
+/* 
+
+
+https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/train/javascript
+MakeUppeerCase
+8 kyu
+
+*/
+
+
+str= 'hello world !';
+
+
+function makeUpperCase(str) {
+
+
+  
+  let text= str.toUpperCase(str);
+  
+  return text;
+   
+  
+  }
