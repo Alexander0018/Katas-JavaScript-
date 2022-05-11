2371d9dc25b3ea3432370d3ff5d72146cd9fd840
Frank Alexander Ardila Tunubala <frankardila@api.docker-mdclaim.com>
2022-05-11 08:01:01 -0500

    ejercicio8

---
+++ ejercicio8.js
@@ -0,0 +1,15 @@
+/*
+
+https://www.codewars.com/kata/59fca81a5712f9fa4700159a/train/javascript
+
+Convert to Binary
+8 kyu
+
+
+*/
+function toBinary(n){
+    var s=(n).toString(2);
+      var n= parseInt((n).toString(2));
+      return n;
+      
+    }
