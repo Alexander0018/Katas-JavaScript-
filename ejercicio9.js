c50c3d1da4aefbdf5c79c035bcf45c68a8258160
Frank Alexander Ardila Tunubala <frankardila@api.docker-mdclaim.com>
2022-05-11 13:47:32 -0500

    ejercicio9

---
+++ ejercicio9.js
@@ -0,0 +1,14 @@
+/*
+
+https://www.codewars.com/kata/511f0fe64ae8683297000001/train/javascript
+
+Basic Training: Add item to an Array
+8 kyu
+
+*/
+
+// add the value "codewars" to the already defined websites array
+
+var websites= [];
+websites.push("codewars");
+
