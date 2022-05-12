363bf21d957167c3c1cbcd1c8183f5de699747cd
Frank Alexander Ardila Tunubala <frankardila@api.docker-mdclaim.com>
2022-05-12 09:54:26 -0500

    ejercicio11

---
+++ ejercicio11.js
@@ -0,0 +1,18 @@
+/*
+
+https://www.codewars.com/kata/5966eeb31b229e44eb00007a/train/javascript
+
+VAPORCODE
+
+7   kyu
+
+
+
+
+
+*/
+
+function vaporcode(string) {
+    return string.toUpperCase().replace(/\s/g, "").split("").join("  ");
+    
+    }
