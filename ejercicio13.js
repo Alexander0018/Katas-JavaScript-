d54ca7342e7112a7a53bb0218f5f83206c22b4fb
Frank Alexander Ardila Tunubala <frankardila@api.docker-mdclaim.com>
2022-05-12 15:39:23 -0500

    ejercicio13

---
+++ ejercicio13.js
@@ -0,0 +1,25 @@
+/*
+
+https://www.codewars.com/kata/55955a48a4e9c1a77500005a/train/javascript
+
+Say hello
+
+7 kyu
+*/
+
+function greet(name) {
+    console.log(name+ '!');
+      
+      if(name== ""){
+        return null;
+      }
+      if(name==null){
+        return null;
+      }
+      else{
+        
+       
+       return 'hello ' +  name+ '!';
+     }
+      
+    }
