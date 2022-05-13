32373f29b733f887f028425bf91486e9d1ddb24f
Frank Alexander Ardila Tunubala <frankardila@api.docker-mdclaim.com>
2022-05-13 11:38:54 -0500

    ejercicio16

---
+++ ejercicio16.js
@@ -0,0 +1,23 @@
+/*
+
+https://www.codewars.com/kata/59827d0c197a17d4cf000028/train/javascript
+Convert Sentences to New York, my guy.
+
+7 kyu
+
+
+
+
+*/
+
+
+function newYorker(text) {
+  
+  
+
+  
+    y = text.replace(/\./g,' my guy.')
+    return y;
+    
+    
+  }
