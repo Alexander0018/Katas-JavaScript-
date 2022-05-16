155aaf024873411563958b13101b6149742cd60d
Frank Alexander Ardila Tunubala <frankardila@api.docker-mdclaim.com>
2022-05-16 11:03:57 -0500

    ejercicio22

---
+++ ejercicio22.js
@@ -0,0 +1,29 @@
+/*
+
+
+https://www.codewars.com/kata/58dbb9b82f449e134a000119/train/javascript
+
+One Line Task: Making Pizza
+
+
+
+* */
+
+
+function makePizza(pieces) { //Let's make n pieces of Pizza ;-)
+    var result = "";
+    while ( pieces-- ){
+      var needSteps = 5;
+      while ( needSteps-- ) result += make( needSteps );
+    }
+    return result;
+  }
+  function make(step){
+    switch ( step +1){
+      case 5: return "P";
+      case 4: return "i";
+      case 3: 
+      case 2: return "z";
+      case 1: return "a";
+    }
+  }
