909521a23766ca72a7a23f6649803d5c20ca72e3
Frank Alexander Ardila Tunubala <frankardila@api.docker-mdclaim.com>
2022-05-16 09:44:15 -0500

    ejercicio21

---
+++ ejercicio21.js
@@ -0,0 +1,25 @@
+/*
+
+https://www.codewars.com/kata/59be6bdc4f98a8a9c700007d/train/javascript
+7 kyu
+
+
+
+
+
+
+
+
+
+*/
+
+function removeChars(s) {
+
+    return s.replace( /[`1203456789~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/g, '');
+  
+  
+    
+  
+  
+  }
+
