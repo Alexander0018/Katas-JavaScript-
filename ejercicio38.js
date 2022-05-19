660d82a7cb8e5d17ffde75c35bfc002d235ccf2a
Frank Alexander Ardila Tunubala <frankardila@api.docker-mdclaim.com>
2022-05-19 08:39:54 -0500

    ejercicio38

---
+++ ejercicio38.js
@@ -0,0 +1,20 @@
+/*
+
+
+https://www.codewars.com/kata/587731fda577b3d1b0001196/train/javascript
+CamelCase Method
+6 kyu
+
+
+
+
+
+
+*/
+
+
+String.prototype.camelCase=function(){
+    let arg = this.toString().trim().split(' ');
+     let arr = arg.map((v,i,arr)=>v?v.slice(0,1).toUpperCase()+v.slice(1):v);
+     return arr.join('');
+   }
