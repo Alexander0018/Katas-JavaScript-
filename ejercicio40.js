802e424bf9f232c07fe26aea0845e5ba477a2794
Frank Alexander Ardila Tunubala <frankardila@api.docker-mdclaim.com>
2022-05-19 11:19:52 -0500

    ejercicio40

---
+++ ejercicio40.js
@@ -0,0 +1,16 @@
+/*
+https://www.codewars.com/kata/59f34ec5a01431ab7600005a/train/javascript
+Factorial length
+6 kyu
+
+
+
+
+*/
+
+
+
+
+function count(n) {
+    return Math.ceil(Math.log10(2*Math.PI*n)/2 + n*Math.log10(n/Math.E))
+    };
